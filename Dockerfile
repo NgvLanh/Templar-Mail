89840efe883e2656161338e1c2f2d3f7df006ed3
Lanh <nguyenlanh121004@gmail.com>
2025-11-17 12:46:55 +0700

    sửa file docker compose và sử jdk của docker file

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,9 @@ RUN mvn dependency:go-offline -B
 COPY src ./src
 RUN mvn clean package -DskipTests
 
-FROM openjdk:17-jdk-alpine
+FROM eclipse-temurin:17-jdk-alpine
 WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
+# COPY .env .env
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "/app/app.jar"]
--- src/main/java/org/me/main/Main.java
+++ src/main/java/org/me/main/Main.java
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
     public static void main(String[] args) {
-    //    Dotenv dotenv = Dotenv.load();
-    //    dotenv.entries().forEach(entry ->
-    //            System.setProperty(entry.getKey(), entry.getValue())
-    //    );
+//        Dotenv dotenv = Dotenv.load();
+//        dotenv.entries().forEach(entry ->
+//                System.setProperty(entry.getKey(), entry.getValue())
+//        );
         SpringApplication.run(Main.class, args);
     }
 
